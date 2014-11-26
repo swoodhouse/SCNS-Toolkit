4b9b6b702ca2b7b3c372c812f8ae6f2e56d9abc3
swoodhouse <sjw229@cam.ac.uk>
2014-11-26 14:51:31 +0000

    Circuit.fs

---
+++ Synthesis Engine/Circuit.fs
@@ -0,0 +1,15 @@
+module Circuit
+
+open FSharpx.Prelude
+
+type Circuit = Value of bool | Node of string | And of Circuit * Circuit | Or of Circuit * Circuit | Not of Circuit
+
+let inhibition pre activ = And (activ, Not pre)
+
+let rec evaluate circuit nodeValues =
+    match circuit with
+    | Value b -> b
+    | And (c1, c2) -> evaluate c1 nodeValues && evaluate c2 nodeValues
+    | Or (c1, c2) -> evaluate c1 nodeValues || evaluate c2 nodeValues
+    | Not c -> not (evaluate c nodeValues)
+    | Node name -> Map.find name nodeValues
