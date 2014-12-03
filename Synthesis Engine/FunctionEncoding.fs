9b26ae8df1d7f40a6a47d24dd7463e1ba3471912
swoodhouse <sjw229@cam.ac.uk>
2014-12-03 11:19:36 +0000

    FunctionEncoding.fs check in

---
+++ Synthesis Engine/FunctionEncoding.fs
@@ -0,0 +1,184 @@
+module FunctionEncoding
+
+open FSharp.Data
+open DataLoading
+open SAT
+
+let rec private delete x = function
+  | [] -> []
+  | h :: t when x = h -> t
+  | h :: t -> h :: delete x t
+
+let private numGenes = let statesFilename = System.Environment.GetCommandLineArgs().[1]
+                       (CsvFile.Load(statesFilename).Headers |> Option.get |> Seq.length) - 1
+
+let private numNonTransitionEnforcedStates = let nonTransitionEnforcedStatesFilename = System.Environment.GetCommandLineArgs().[5]
+                                             readLines nonTransitionEnforcedStatesFilename |> Array.length
+
+let [<Literal>] AND = 0
+let [<Literal>] OR = 1
+let NOTHING = numGenes + 2
+
+let makeCircuitVar = let numBits = (uint32 << ceil <| System.Math.Log(float numGenes, 2.0)) + 1u
+                     fun name -> BitVec (name, numBits)
+                     
+let makeEnforcedVar = let numBits = (uint32 << ceil <| System.Math.Log(float numNonTransitionEnforcedStates, 2.0)) + 1u
+                      fun name -> BitVec (name, numBits)
+
+let private variableDomains var lowerBound upperBound =
+    (var >=. lowerBound) &&. (var <=. upperBound)
+
+let private parentsOfNothingArentGates (a : BitVec []) (r : BitVec []) =
+    let f c1 c2 p = ((c1 =. NOTHING) ||. (c2 =. NOTHING)) =>. ((p <>. AND) &&. (p <>. OR))
+
+    let aParents = And [| Implies ((a.[1] =. NOTHING) ||. (a.[2] =. NOTHING),  And [| a.[0] <>. AND; a.[0] <>. OR; a.[0] <>. NOTHING |])
+                          f a.[3] a.[4] a.[1]
+                          f a.[5] a.[6] a.[2] |]
+
+    let rParents = And [| f r.[1] r.[2] r.[0]
+                          f r.[3] r.[4] r.[1]
+                          f r.[5] r.[6] r.[2] |]
+                              
+    aParents &&. rParents
+
+let private parentsOfRestAreGates (a : BitVec []) (r : BitVec []) =
+    let f c1 c2 p = ((c1 <>. NOTHING) ||. (c2 <>. NOTHING)) =>.  ((p =. AND) ||. (p =. OR))
+
+    let aParents = And [| f a.[1] a.[2] a.[0]
+                          f a.[3] a.[4] a.[1]
+                          f a.[5] a.[6] a.[2] |]
+
+    let rParents = And [| f r.[1] r.[2] r.[0]
+                          f r.[3] r.[4] r.[1]
+                          f r.[5] r.[6] r.[2] |]
+
+    aParents &&. rParents
+
+let private variablesDoNotAppearMoreThanOnce symVars =
+    let isVar (v : BitVec) = And [| v <>. NOTHING; v <>. AND; v <>. OR |]
+    let notEqual v vars = let a = List.map ((<>.) v) vars
+                          a |> And
+    let doesNotAppearMoreThanOnce (v : BitVec) = 
+        (isVar v) =>. (notEqual v (delete v symVars))
+    Seq.map doesNotAppearMoreThanOnce symVars |> And
+
+let private enforceSiblingLexigraphicalOrdering (v1 : BitVec) (v2 : BitVec) =
+    v1 <=. v2
+
+let private enforceLexigraphicalOrderingBetweenBranches (p1 : BitVec) (p2 : BitVec) (c1 : BitVec) (c2 : BitVec) =
+    (p1 =. p2) =>. (c1 <=. c2)
+
+let private enforceLexigraphicalOrderingNaryGate (vars : BitVec []) =
+    (vars.[0] =. vars.[1]) =>. (vars.[2] <=. vars.[3])
+
+let activatorVars = set [ for i in 1..7 do yield "a" + string i ]
+let repressorVars = set [ for i in 1..7 do yield "r" + string i ]
+let circuitVars = activatorVars + repressorVars
+
+let private fixMaxInputs v max =
+    match max with
+    | 0 -> makeCircuitVar (v + "1") =. NOTHING
+    | 1 -> makeCircuitVar (v + "2") =. NOTHING
+    | 2 -> makeCircuitVar (v + "4") =. NOTHING
+    | 3 -> makeCircuitVar (v + "6") =. NOTHING
+    | _ -> True
+
+let private fixMaxActivators = fixMaxInputs "a"
+let private fixMaxRepressors = fixMaxInputs "r"
+
+let encodeUpdateFunction gene genes maxActivators maxRepressors =
+    if not (Set.contains gene genes && maxActivators > 0 && maxActivators <= 4 && maxRepressors >= 0 && maxRepressors <= 4) then
+        failwith "Incorrect arguments to encodeForUpdateFunction"
+
+    let a = [| for i in 1..7 -> makeCircuitVar (sprintf "a%i" i) |]
+    let r = [| for i in 1..7 -> makeCircuitVar (sprintf "r%i" i) |]
+    let symVars = a ++ r
+
+    let circuitEncoding = And <| [| variableDomains a.[0] 0 (NOTHING - 1)
+                                    variableDomains r.[0] 0 NOTHING
+
+                                    variableDomains a.[1] 0 NOTHING; variableDomains a.[2] 0 NOTHING              
+                                    variableDomains r.[1] 0 NOTHING; variableDomains r.[2] 0 NOTHING
+
+                                    variableDomains a.[3] 2 NOTHING; variableDomains a.[4] 2 NOTHING; variableDomains a.[4] 2 NOTHING; variableDomains a.[5] 2 NOTHING
+                                    variableDomains a.[6] 2 NOTHING
+                                    variableDomains r.[3] 2 NOTHING; variableDomains r.[4] 2 NOTHING; variableDomains r.[4] 2 NOTHING; variableDomains r.[5] 2 NOTHING
+                                    variableDomains r.[6] 2 NOTHING
+
+                                    parentsOfNothingArentGates a r
+                                    parentsOfRestAreGates a r
+                                    variablesDoNotAppearMoreThanOnce (List.ofSeq <| a ++ r)
+                                    
+                                    And [| for i in 1 .. 2 .. 5 -> enforceSiblingLexigraphicalOrdering a.[i] a.[i + 1] |]
+                                    And [| for i in 1 .. 2 .. 5 -> enforceSiblingLexigraphicalOrdering r.[i] r.[i + 1] |]
+                                    
+                                    enforceLexigraphicalOrderingBetweenBranches a.[1] a.[2] a.[3] a.[5]
+                                    enforceLexigraphicalOrderingBetweenBranches r.[1] r.[2] r.[3] r.[5]
+
+                                    enforceLexigraphicalOrderingNaryGate a
+                                    enforceLexigraphicalOrderingNaryGate r
+                                    
+                                    fixMaxActivators maxActivators
+                                    fixMaxRepressors maxRepressors |]
+    (circuitEncoding, symVars)
+ 
+let private evaluateUpdateFunction = 
+    let counter = ref 0
+    
+    fun (symVars : BitVec []) (geneValues : bool []) ->
+        let i = !counter
+        counter := i + 1
+
+        let geneValues = Array.map toBool geneValues
+        
+        let intermediateValueVariablesA = [| for j in 1 .. 7 ->  Bool <| sprintf "va%i_%i" j i |]
+        let intermediateValueVariablesR = [| for j in 1 .. 7 ->  Bool <| sprintf "vr%i_%i" j i |]
+
+        let andConstraints (variables : Bool []) pi c1i c2i =
+            Implies (symVars.[pi] =. AND, variables.[pi] =. (variables.[c1i] &&. variables.[c2i]))
+
+        let orConstraints (variables : Bool []) pi c1i c2i =
+            Implies (symVars.[pi] =. OR, variables.[pi] =. (variables.[c1i] ||. variables.[c2i]))
+        
+        let variableConstraints (variables : Bool []) =
+            let f i symVar =
+                [| for v in 2 .. (NOTHING - 1) do
+                        yield Implies (symVar =. v, variables.[i] =. geneValues.[v - 2])
+                |] |> And
+
+            Array.mapi f symVars |> And
+
+        let circuitValue =
+            let noRepressors = symVars.[7] =. NOTHING
+            If (noRepressors,
+                intermediateValueVariablesA.[0],
+                intermediateValueVariablesA.[0] &&. Not intermediateValueVariablesR.[0])
+
+        let circuitVal = Bool <| sprintf "circuit_%i" i
+                        
+        (And [| variableConstraints intermediateValueVariablesA
+                variableConstraints intermediateValueVariablesR
+                andConstraints intermediateValueVariablesA 0 1 2
+                andConstraints intermediateValueVariablesA 1 3 4
+                andConstraints intermediateValueVariablesA 2 5 6
+                andConstraints intermediateValueVariablesR 0 1 2
+                andConstraints intermediateValueVariablesR 1 3 4
+                andConstraints intermediateValueVariablesR 2 5 6
+                orConstraints intermediateValueVariablesA 0 1 2
+                orConstraints intermediateValueVariablesA 1 3 4
+                orConstraints intermediateValueVariablesA 2 5 6
+                orConstraints intermediateValueVariablesR 0 1 2
+                orConstraints intermediateValueVariablesR 1 3 4
+                orConstraints intermediateValueVariablesR 2 5 6
+
+                circuitVal =. circuitValue|], circuitVal)
+
+let circuitEvaluatesToSame gene symVars (profile : bool []) =
+    let b = toBool profile.[gene - 2]
+    let evaluationEncoding, circuitVal = evaluateUpdateFunction symVars profile
+    (evaluationEncoding, circuitVal =. b)
+            
+let circuitEvaluatesToDifferent gene symVars (profile : bool []) =
+    let b = toBool profile.[gene - 2]
+    let evaluationEncoding, circuitVal = evaluateUpdateFunction symVars profile
+    (evaluationEncoding, circuitVal =. Not b)
