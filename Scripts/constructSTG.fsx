92472b053d1fad3aa9e7db022c167e506593c730
Steven Woodhouse <sjw229@cam.ac.uk>
2015-08-28 14:06:16 +0100

    Fixed bug in constructSTG.fsx on data sets with 64 genes

--- Scripts/constructSTG.fsx
+++ Scripts/constructSTG.fsx
@@ -49,32 +49,29 @@ let boolArrayToUint64 a =
 
 let toUniqueBitvectors rows =
     let seen = HashSet<uint64>()
-    Map.ofList [ for (id, array) in rows do
+    Map.ofList [ for (id : string, array) in rows do
                      let bitVec = boolArrayToUint64 array
                      if not <| seen.Contains bitVec then
                          yield (bitVec, id) ]
 
-let possibleOneNeighbours numGenes state =
+let genes, rows = loadCsv inputCsvFilename
+let states = discretise rows
+let map = toUniqueBitvectors states
+let numGenes = Array.length genes
+
+let allOnes = if numGenes = 64 then System.UInt64.MaxValue else pown 2UL numGenes - 1UL
+
+let possibleOneNeighbours state =
     let flipRightmost0 i = i ||| i + 1UL
     let flip = ref state
-    seq { while !flip <> (pown 2UL numGenes) - 1UL do
+    seq { while !flip <> allOnes do
               let newFlip = flipRightmost0 !flip
               yield (newFlip ^^^ !flip) ||| state
               flip := newFlip }
 
-let geneChange genes state state' =
-    let index = Array.length genes - (int <| System.Math.Log(float <| state ^^^ state', 2.0)) - 1
+let geneChange (state : uint64) state' =
+    let index = numGenes - (int <| System.Math.Log(float <| state ^^^ state', 2.0)) - 1
     genes.[index]
-
-let constructSTG genes map =
-    seq { for state in Map.keys map do
-              for state' in possibleOneNeighbours (Array.length genes) state do
-                  if Map.containsKey state' map then
-                      yield (Map.find state map, Map.find state' map, geneChange genes state state') }
-                      
-let header, rows = loadCsv inputCsvFilename
-let states = discretise rows
-let map = toUniqueBitvectors states 
 
 let uniqueStates =
     let uniqueIds = Set.ofSeq <| Map.values map
@@ -82,12 +79,16 @@ let uniqueStates =
            if Set.contains id uniqueIds then
              yield (id, a) }
 
-let edges = constructSTG header map
-
+let edges =
+    seq { for state in Map.keys map do
+              for state' in possibleOneNeighbours state do
+                  if Map.containsKey state' map then
+                      yield (Map.find state map, Map.find state' map, geneChange state state') }
+    
 let writeBoolArray a =
     Array.map (sprintf ",%b") a |> Array.reduce (+)
 
-System.IO.File.WriteAllLines(outputStatesCsvFilename, let header = header |> Array.map (sprintf ",%s") |> Array.reduce (+)
+System.IO.File.WriteAllLines(outputStatesCsvFilename, let header = genes |> Array.map (sprintf ",%s") |> Array.reduce (+)
                                                       in let rows = uniqueStates |> Seq.map (fun (id, a) -> sprintf "%s%s" id (writeBoolArray a))
                                                          in Seq.append [header] rows)
 System.IO.File.WriteAllLines(outputEdgesCsvFilename, let header = "Gene,StateA,StateB"
